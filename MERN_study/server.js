e5e3fb3cd59a8aea4376bab3a4b6b81143c2eb30
KOKOPA <52780289+KOKOPA@users.noreply.github.com>
2019-09-17 13:49:48 +0900

    router 추가

--- MERN_study/server.js
+++ MERN_study/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
+const router = express.Router();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
@@ -17,6 +18,8 @@ mongoose.connect('mongodb://localhost/mongodb_tutorial');
 
 const Customer = require('./models/customer');
 
-require('./routes')(app, Customer);
+require('./routes')(router, Customer);
+
+app.use('/', router);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
